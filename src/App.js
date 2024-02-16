1472ec7914bfea93f50509953bcba3dc3f34528b
Jyles Mary <jylesjiss@gmail.com>
2024-02-16 13:08:47 +0530

    simple-interest-cal

--- src/App.js
+++ src/App.js
@@ -1,23 +1,58 @@
-import logo from './logo.svg';
+
 import './App.css';
+import TextField from '@mui/material/TextField';
+import Button from '@mui/material/Button';
+import { useState } from 'react';
 
 function App() {
+  const [amount,setAmount]=useState(0);
+  const [year,setYear]=useState(0);
+  const [rate,setRate]=useState(0);
+
+
+  const [interest,setInterest]=useState(0);
+  console.log(amount,year,rate);
+
+  const calculate=(e)=>{
+    const output = amount*year*rate/100;
+    console.log(output);
+    setInterest(output)
+  }
+
+  const reset=(e)=>{
+    setAmount(0)
+    setYear(0)
+    setRate(0)
+    setInterest(0)
+  }
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className='container'>
+        <div className='header'>
+          <h1><u>Simple Interest Calculator</u></h1>
+          <p>calculate your simple interest with us</p>
+        </div>
+        <div className='total'>
+          <h2> &#8377; {interest}</h2>
+          <p>Your Total Interest</p>
+        </div>
+        <div className='form'>
+          <form autoComplete='off'>
+            <div className='input'>
+              <TextField id="outlined-basic"  label="Amount" value={amount||""} onChange={(e)=>setAmount(e.target.value)} variant="outlined" />
+              <br/>
+              <TextField id="filled-basic" label="Year" value={year||""} onChange={(e)=>setYear(e.target.value)} variant="outlined" />
+              <br/>
+              <TextField id="standard-basic" label="Rate" value={rate||""} onChange={(e)=>setRate(e.target.value)} variant="outlined" />
+            </div>
+          </form>
+        </div>
+        <div className='button'>
+          <Button variant="contained" onClick={e=>calculate(e)}>Calculate</Button>
+          <Button variant="contained" onClick={e=>reset(e)}>Reset</Button>
+        </div>
+      </div>
     </div>
   );
 }
